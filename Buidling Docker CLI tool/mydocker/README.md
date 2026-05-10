1de32bc0efa6e875f451bd96684da6d24e4ae2ce
Andrew White <white.drew@gmail.com>
2026-05-10 09:01:51 -0500

    chore: keepalive 2026-05-10

---
+++ Buidling Docker CLI tool/mydocker/README.md
@@ -0,0 +1,9 @@
+## Command Structure
+
+```sh
+mydocker list containers        # List all containers
+mydocker list images            # List all Docker images
+mydocker start <container_id>   # Start a specific container
+mydocker stop <container_id>    # Stop a specific container
+mydocker pull <image_name>      # Pull a new Docker image
+```
---
+++ Buidling Docker CLI tool/mydocker/src/docker.rs
@@ -0,0 +1,72 @@
+use bollard::container::{ListContainersOptions, StartContainerOptions, StopContainerOptions};
+use bollard::errors::Error;
+use bollard::image::{CreateImageOptions, ListImagesOptions};
+use bollard::models::{ContainerSummary, ImageSummary};
+use bollard::Docker;
+use futures_util::TryStreamExt;
+pub struct DockerClient {
+    docker: Docker,
+}
+
+impl DockerClient {
+    pub fn new() -> Self {
+        let docker = Docker::connect_with_unix(
+            "~/.docker/run/docker.sock",
+            120, // Timeout in seconds
+            bollard::API_DEFAULT_VERSION,
+        )
+        .expect("Failed to connect to Docker");
+
+        Self { docker: docker }
+    }
+
+    pub async fn list_containers(&self, all: bool) -> Result<Vec<ContainerSummary>, Error> {
+        let options = Some(ListContainersOptions::<String> {
+            all,
+            ..Default::default()
+        });
+
+        let containers = self.docker.list_containers(options).await?;
+        Ok(containers)
+    }
+
+    pub async fn list_images(&self) -> Result<Vec<ImageSummary>, Error> {
+        let options = Some(ListImagesOptions::<String> {
+            all: true,
+            ..Default::default()
+        });
+
+        let images = self.docker.list_images(options).await?;
+        Ok(images)
+    }
+
+    pub async fn start_container(&self, container_name: &str) -> Result<(), Error> {
+        self.docker
+            .start_container(container_name, None::<StartContainerOptions<String>>)
+            .await?;
+        Ok(())
+    }
+
+    pub async fn stop_container(&self, container_name: &str) -> Result<(), Error> {
+        let options = Some(StopContainerOptions { t: 30 });
+        self.docker.stop_container(container_name, options).await?;
+        Ok(())
+    }
+
+    pub async fn pull_image(&self, image_name: &str) -> Result<(), Error> {
+        let options = Some(CreateImageOptions {
+            from_image: image_name,
+            ..Default::default()
+        });
+
+        let mut stream = self.docker.create_image(options, None, None);
+
+        while let Some(msg) = stream.try_next().await? {
+            if let Some(status) = msg.status {
+                println!("{}", status);
+            }
+        }
+
+        Ok(())
+    }
+}
